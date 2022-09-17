9bba0b1badf2a314e08384efd9fbf95b42cf12e5
Ramkumar0302 <113343857+Ramkumar0302@users.noreply.github.com>
2022-09-17 15:24:33 +0530

    import java.util.*; class Sample {     public static boolean isPrime(int n)     {         if (n==1)         return false;
            {
                for(int i=2;i<=(int)Math.sqrt(n);i++)
                {
                    if(n%i==0)
                    return false;
                    
                }
                
            }
            return true;
        }
            public static void main(String args[])
            {
                Scanner sc=new Scanner(System.in);
                int n,x[],i,count=0,k;
                n=sc.nextInt();
                x=new int[n];
                for(i=0;i<n;i++)
                    x[i]=sc.nextInt();
                    k=sc.nextInt();
                
                for(i=0;i<n;i++)
                {
                    if(isPrime(x[i]))
                    {
                        if(x[i]>=k)
                        count++;
                    }
                }
                System.out.println(count);
            
        }
    }

---
+++ Primes_greater_than_k.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Sample
+{
+    public static boolean isPrime(int n)
+    {
+        if (n==1)
+        return false;
+        
+        {
+            for(int i=2;i<=(int)Math.sqrt(n);i++)
+            {
+                if(n%i==0)
+                return false;
+                
+            }
+            
+        }
+        return true;
+    }
+        public static void main(String args[])
+        {
+            Scanner sc=new Scanner(System.in);
+            int n,x[],i,count=0,k;
+            n=sc.nextInt();
+            x=new int[n];
+            for(i=0;i<n;i++)
+                x[i]=sc.nextInt();
+                k=sc.nextInt();
+            
+            for(i=0;i<n;i++)
+            {
+                if(isPrime(x[i]))
+                {
+                    if(x[i]>=k)
+                    count++;
+                }
+            }
+            System.out.println(count);
+        
+    }
+}
