6ebc92e7ca63ca63494fa9f25575d527bcb460f7
Ramkumar <113343857+Ramkumar0302@users.noreply.github.com>
2022-10-03 07:35:21 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=0,b=1,c,n;         n=sc.nextInt();         System.out.print(a+" "+b+" ");        for(int i=2;i<n;i++)
            {
                c=a+b;
                a=b;
                b=c;
                System.out.print(c+" ");
            }
        }
    }

---
+++ fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=0,b=1,c,n;
+        n=sc.nextInt();
+        System.out.print(a+" "+b+" ");
+       for(int i=2;i<n;i++)
+     
+        {
+            c=a+b;
+            a=b;
+            b=c;
+            System.out.print(c+" ");
+        }
+    }
+}
