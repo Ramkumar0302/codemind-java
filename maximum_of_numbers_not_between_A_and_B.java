f62fcf8553284986a02fd00573f38e44929afdd5
Ramkumar <113343857+Ramkumar0302@users.noreply.github.com>
2022-10-07 17:08:23 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,max,a,b,flag=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)            x[i]=sc.nextInt();            a=sc.nextInt();            b=sc.nextInt();            max=x[0];            for(i=0;i<n;i++)            {               if(x[i]<a || x[i]>b)               {                if(x[i]>max)                max=x[i];                flag=1;                }            }         //   System.out.println(max);     if(flag==0)     System.out.println(-1);     else     System.out.println(max);     } }

---
+++ maximum_of_numbers_not_between_A_and_B.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i,max,a,b,flag=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+           x[i]=sc.nextInt();
+           a=sc.nextInt();
+           b=sc.nextInt();
+           max=x[0];
+           for(i=0;i<n;i++)
+           {
+              if(x[i]<a || x[i]>b)
+              {
+               if(x[i]>max)
+               max=x[i];
+               flag=1;
+               }
+           }
+        //   System.out.println(max);
+    if(flag==0)
+    System.out.println(-1);
+    else
+    System.out.println(max);
+    }
+}
