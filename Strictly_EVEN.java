fc6bb0d1a29c10385c154acf850982097a1fb07e
Ramkumar <113343857+Ramkumar0302@users.noreply.github.com>
2022-11-03 19:24:46 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,count=0,c=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             if(x[i]%2==0 && i%2==0)             {                 count++;             }             if(x[i]%2==0)             c++;         }         if(c==count)         System.out.println("True");         else         System.out.println("False");
        }
    }

---
+++ Strictly_EVEN.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i,count=0,c=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(x[i]%2==0 && i%2==0)
+            {
+                count++;
+            }
+            if(x[i]%2==0)
+            c++;
+        }
+        if(c==count)
+        System.out.println("True");
+        else
+        System.out.println("False");
+        
+    }
+}
