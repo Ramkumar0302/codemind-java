2b5901957c720826d11c304ce0a5f25a10837d06
Ramkumar <113343857+Ramkumar0302@users.noreply.github.com>
2022-11-06 21:06:21 +0530

    import java.util.*; class Sample {     public static boolean isPrime(int n)     {         if(n==1)         return false;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             return false;         }         return true;     }
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int n,x[],i,sum=0,count=0;
            n=sc.nextInt();
            x=new int[n];
            for(i=0;i<n;i++)
            x[i]=sc.nextInt();
            for(i=0;i<n;i++)
            {
                if(isPrime(x[i]))
                {
                count++;
                }
            }
            System.out.println(count);
        }
    }

--- Primes_in_the_array.java
+++ Primes_in_the_array.java
@@ -4,30 +4,29 @@ class Sample
     public static boolean isPrime(int n)
     {
         if(n==1)
-             return false;
+        return false;
         for(int i=2;i<=(int)Math.sqrt(n);i++)
         {
             if(n%i==0)
-               return false;
+            return false;
         }
         return true;
-        
     }
+    
     public static void main(String args[])
     {
-        Scanner sc=new Scanner (System.in);
-        int n,x[],i,count=0;
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i,sum=0,count=0;
         n=sc.nextInt();
         x=new int[n];
-        
         for(i=0;i<n;i++)
-        {
-            x[i]=sc.nextInt();
-        }
+        x[i]=sc.nextInt();
         for(i=0;i<n;i++)
         {
             if(isPrime(x[i]))
+            {
             count++;
+            }
         }
         System.out.println(count);
     }
