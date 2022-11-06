0d37dc910838a0e34156b039eb607af4cb658068
Ramkumar <113343857+Ramkumar0302@users.noreply.github.com>
2022-11-06 11:27:38 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,count=0,c=0,sum=0,found=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             count=1;             if(x[i]!=-99)             {                for(int j=0;j<n;j++)                {                    if(x[i]==x[j] && i!=j)                    {                        x[j]=-99;                        count++;                    }                }                if(x[i]%2==0)                c++;
                   }
                }
            System.out.println(c);
        }
    }

--- unique_even_count.java
+++ unique_even_count.java
@@ -4,29 +4,29 @@ class Sample
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],i,j,count=0,sum=0;
+        int n,x[],i,count=0,c=0,sum=0,found=0;
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
         x[i]=sc.nextInt();
         for(i=0;i<n;i++)
         {
-           count=1;
-           if(x[i]!=-99)
-        {
-    
-        for(j=0;j<n;j++)
-        {
-            if(x[i]==x[j] && i!=j)
+            count=1;
+            if(x[i]!=-99)
             {
-            count++;
-            x[j]=-99;
+               for(int j=0;j<n;j++)
+               {
+                   if(x[i]==x[j] && i!=j)
+                   {
+                       x[j]=-99;
+                       count++;
+                   }
+               }
+               if(x[i]%2==0)
+               c++;
+            
+               }
             }
-        }
-        if(x[i]%2==0)
-        sum++;
-        }
-        }
-        System.out.println(sum);
-  }
+        System.out.println(c);
+    }
 }
