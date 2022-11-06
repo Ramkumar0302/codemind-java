e4c76fa235df8af6cf45869482abec6b777d23f7
Ramkumar <113343857+Ramkumar0302@users.noreply.github.com>
2022-11-06 22:10:39 +0530

    import java.util.*; class Sample {     public static boolean isPalin(int n)     {         int rev=0,temp=n;         while(n!=0)         {             rev=rev*10+n%10;             n=n/10;         }         if(temp==rev)         return true;         else         return false;
        }
        
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int n,x[],i,count=0;
            n=sc.nextInt();
            x=new int[n];
            for(i=0;i<n;i++)
            {
                x[i]=sc.nextInt();
            }
            for(i=0;i<n;i++)
            {
               if(isPalin(x[i]))
               {
               count++;
               }
            }
            System.out.println(count);
        }
    }

--- Count_palindromes.java
+++ Count_palindromes.java
@@ -3,36 +3,36 @@ class Sample
 {
     public static boolean isPalin(int n)
     {
-        int temp=n;
-        int rev=0;
+        int rev=0,temp=n;
         while(n!=0)
         {
             rev=rev*10+n%10;
             n=n/10;
-            
         }
         if(temp==rev)
         return true;
         else
         return false;
+        
     }
+    
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int x[],i,n,count=0;
-        
+        int n,x[],i,count=0;
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
         {
             x[i]=sc.nextInt();
-            
         }
-        for(i=0;i<n;i++){
-          if(isPalin(x[i]))
-            count++;
+        for(i=0;i<n;i++)
+        {
+           if(isPalin(x[i]))
+           {
+           count++;
+           }
         }
         System.out.println(count);
-        
     }
 }
