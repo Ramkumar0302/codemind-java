0e4a5edcb4afd74a99998cd43669735bb5ae8678
Ramkumar <113343857+Ramkumar0302@users.noreply.github.com>
2022-11-06 11:22:50 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,count=0,c=0,sum=0,found=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             count=1;             if(x[i]!=-99)             {                for(int j=0;j<n;j++)                {                    if(x[i]==x[j] && i!=j)                    {                        x[j]=-99;                        count++;                    }                }              if(x[i]%2!=0)              sum=sum+x[i];
                }
            }
            System.out.println(sum);
        }
    }

--- unique_odd_elements_sum.java
+++ unique_odd_elements_sum.java
@@ -4,34 +4,29 @@ class Sample
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],i,j,count=0,sum=0;
+        int n,x[],i,count=0,c=0,sum=0,found=0;
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
         x[i]=sc.nextInt();
         for(i=0;i<n;i++)
         {
-           count=1;
-           if(x[i]!=-99)
-        {
-    
-        for(j=0;j<n;j++)
-        {
-            if(x[i]==x[j] && i!=j)
+            count=1;
+            if(x[i]!=-99)
             {
-            count++;
-            x[j]=-99;
+               for(int j=0;j<n;j++)
+               {
+                   if(x[i]==x[j] && i!=j)
+                   {
+                       x[j]=-99;
+                       count++;
+                   }
+               }
+             if(x[i]%2!=0)
+             sum=sum+x[i];
+            
             }
         }
-        if(x[i]%2!=0)
-        
-        sum=sum+x[i];
-        }
-        }
-        
-        
         System.out.println(sum);
-        
-        
-  }
+    }
 }
