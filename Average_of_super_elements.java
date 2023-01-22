d322cb6abb78e0d68b93083c715ccd726a316f7f
Ramkumar <113343857+Ramkumar0302@users.noreply.github.com>
2023-01-22 10:43:52 +0530

    import java.util.*; import java.util.Map.*; class Sample {     public static float Output(int arr[])     {         int sum=0,count=0;         HashMap<Integer,Integer> hm=new HashMap<>();         for(int i=0;i<arr.length;i++)         {             hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);         }             for(Map.Entry entry:hm.entrySet())             {                 if((int)entry.getKey()==(int)entry.getValue())                 {                 sum=sum+(int)entry.getKey();                 count++;                 }             }
            if(count==0)
            return -1;
            else
            return (float)sum/count;
        }
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int x[],n;
            n=sc.nextInt();
            x=new int[n];
            for(int i=0;i<n;i++)
            {
                x[i]=sc.nextInt();
            }
            
            float res=Output(x);
            if(res<0)
            System.out.println("-1");
            else
            System.out.format("%.2f",res);
        }
    }

--- Average_of_super_elements.java
+++ Average_of_super_elements.java
@@ -1,45 +1,44 @@
-import java.util.Scanner;
-class Sample 
+import java.util.*;
+import java.util.Map.*;
+class Sample
 {
-
- public static void main(String[] args) 
-{
-// TODO Auto-generated method stu
-int n,i,j,x[],count=0,flag=0,sum=0,found=0;
-Scanner sc=new Scanner(System.in);
-n=sc.nextInt();
-x=new int[n];
-for(i=0;i<n;i++)
-x[i]=sc.nextInt();
-int max=0;
-for(i=0;i<n-1;i++)
-
-for(i=0;i<n;i++)
-{
-count=1;
-if(x[i]!=-99)
-{
-for(j=0;j<n;j++)
-{
-if(x[i]==x[j] && i!=j)
-{
-count++;
-x[j]=-99;
-}
-}
-}
-if(x[i]==count)
-{
-  sum=sum+x[i];
-  flag++;
-  found=1;
-}
-
-
-}
-if(found==1)
-System.out.format("%.2f",(float)sum/flag);
-else
-System.out.print("-1");
-}
+    public static float Output(int arr[])
+    {
+        int sum=0,count=0;
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+        }
+            for(Map.Entry entry:hm.entrySet())
+            {
+                if((int)entry.getKey()==(int)entry.getValue())
+                {
+                sum=sum+(int)entry.getKey();
+                count++;
+                }
+            }
+        
+        if(count==0)
+        return -1;
+        else
+        return (float)sum/count;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x[],n;
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        
+        float res=Output(x);
+        if(res<0)
+        System.out.println("-1");
+        else
+        System.out.format("%.2f",res);
+    }
 }
